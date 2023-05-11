0cc5787b56c51c491d73eb0a6c35f8d8bcd9fd8d
Anik Das <onikd08@gmail.com>
2023-05-11 04:41:47 +0300

    Add delete method

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const users = useLoaderData();
+  const [displayUsers, setDisplayUsers] = useState(users);
+  console.log(displayUsers);
+
   const handleDeleteUser = (user) => {
     const agree = window.confirm(
       `Are you sure you want to delete the user: ${user.name}`
     );
     if (agree) {
-      console.log("deleting user...");
+      fetch(`http://localhost:8000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount) {
+            //alert("User is deleted");
+            const remainingUsers = displayUsers.filter(
+              (displayUser) => displayUser._id !== user._id
+            );
+            setDisplayUsers(remainingUsers);
+          }
+        });
     }
-    console.log(user._id);
   };
   return (
     <div>
       <h3>This is home</h3>
-      <h2>Users:{users?.length} </h2>
+      <h2>Users:{displayUsers?.length} </h2>
       <div>
-        {users.map((user, idx) => (
+        {displayUsers.map((user, idx) => (
           <p key={idx}>
             {user.name} {user.email}
             <button onClick={() => handleDeleteUser(user)}>X</button>
